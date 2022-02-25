bca24e40339f6fc86a2d2ceeadf4be5bfe7aae27
smin9408 <stmdaks888@naver.com>
2022-02-25 12:11:58 +0900

    Scanner로 숫자 입력받기 로직

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,8 +1,18 @@
 package codes;
 
+import java.util.Scanner;
+
 public class MainDrive {
 
 	public static void main(String[] args) {
-		
+
+//		6개의 숫자 입력받기.
+		Scanner sc = new Scanner(System.in);
+
+		for (int i = 0; i < 6; i++) {
+			System.out.print(i + 1 + "번째 숫자 : ");
+			int inputNum = sc.nextInt();
+		}
+
 	}
 }
